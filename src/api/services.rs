d0d0a983e68e49234cde4241ed9c708ef7f43493
Linus Kirkwood <linuskirkwood@gmail.com>
2023-09-13 15:54:19 +1000

    Added uri_export parameters

--- src/api/services.rs
+++ src/api/services.rs
@@ -51,7 +51,81 @@ impl PSServer {
     }
 
     /// Returns the pageseeder thread that is exporting the URI(s).
-    pub async fn uri_export(&mut self, member: &str, uri: &str) -> PSResult<Thread> {
+    pub async fn uri_export(
+        &mut self,
+        member: &str,
+        uri: &str,
+        binary_metadata_only: Option<bool>,
+        compare: Option<String>,
+        context: Option<String>,
+        excludes: Option<Vec<String>>,
+        fail_on_error: Option<bool>,
+        forward_depth: Option<u64>,
+        includes: Option<Vec<String>>,
+        load_alternates: Option<bool>,
+        load_images: Option<bool>,
+        metadata_only: Option<bool>,
+        publication_id: Option<String>,
+        version: Option<String>,
+        reverse_depth: Option<u64>,
+        since: Option<String>,
+        with: Option<Vec<String>>,
+        xref_types: Option<Vec<String>>,
+    ) -> PSResult<Thread> {
+        let mut params = vec![];
+        // TODO get away from this (awful!)
+        if binary_metadata_only.is_some() {
+            params.push((
+                "binary-metadata-only",
+                binary_metadata_only.unwrap().to_string(),
+            ));
+        }
+        if compare.is_some() {
+            params.push(("compare", compare.unwrap().to_string()));
+        }
+        if context.is_some() {
+            params.push(("context", context.unwrap().to_string()));
+        }
+        if excludes.is_some() {
+            params.push(("excludes", excludes.unwrap().join(",").to_string()));
+        }
+        if fail_on_error.is_some() {
+            params.push(("fail-on-error", fail_on_error.unwrap().to_string()));
+        }
+        if forward_depth.is_some() {
+            params.push(("forward-depth", forward_depth.unwrap().to_string()));
+        }
+        if includes.is_some() {
+            params.push(("includes", includes.unwrap().join(",").to_string()));
+        }
+        if load_alternates.is_some() {
+            params.push(("load-alternates", load_alternates.unwrap().to_string()));
+        }
+        if load_images.is_some() {
+            params.push(("load-images", load_images.unwrap().to_string()));
+        }
+        if metadata_only.is_some() {
+            params.push(("metadata-only", metadata_only.unwrap().to_string()));
+        }
+        if publication_id.is_some() {
+            params.push(("publicationid", publication_id.unwrap().to_string()));
+        }
+        if version.is_some() {
+            params.push(("version", version.unwrap().to_string()));
+        }
+        if reverse_depth.is_some() {
+            params.push(("reverse-depth", reverse_depth.unwrap().to_string()));
+        }
+        if since.is_some() {
+            params.push(("since", since.unwrap().to_string()));
+        }
+        if with.is_some() {
+            params.push(("with", with.unwrap().join(",").to_string()));
+        }
+        if xref_types.is_some() {
+            params.push(("xref-types", xref_types.unwrap().join(",").to_string()));
+        }
+
         let resp = self
             .checked_get(&Service::UriExport { member, uri }.url_path(), None, None)
             .await?;
