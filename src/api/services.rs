b8c045ad016004c36eb28a503b9f4579782b9f0d
Linus Kirkwood <linuskirkwood@gmail.com>
2023-09-13 16:03:23 +1000

    Moved to manual param passing until better fix

--- src/api/services.rs
+++ src/api/services.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use quick_xml::de;
 use reqwest::Response;
 use serde::de::DeserializeOwned;
@@ -55,79 +57,14 @@ impl PSServer {
         &mut self,
         member: &str,
         uri: &str,
-        binary_metadata_only: Option<bool>,
-        compare: Option<String>,
-        context: Option<String>,
-        excludes: Option<Vec<String>>,
-        fail_on_error: Option<bool>,
-        forward_depth: Option<u64>,
-        includes: Option<Vec<String>>,
-        load_alternates: Option<bool>,
-        load_images: Option<bool>,
-        metadata_only: Option<bool>,
-        publication_id: Option<String>,
-        version: Option<String>,
-        reverse_depth: Option<u64>,
-        since: Option<String>,
-        with: Option<Vec<String>>,
-        xref_types: Option<Vec<String>>,
+        params: &Vec<(&str, &str)>,
     ) -> PSResult<Thread> {
-        let mut params = vec![];
-        // TODO get away from this (awful!)
-        if binary_metadata_only.is_some() {
-            params.push((
-                "binary-metadata-only",
-                binary_metadata_only.unwrap().to_string(),
-            ));
-        }
-        if compare.is_some() {
-            params.push(("compare", compare.unwrap().to_string()));
-        }
-        if context.is_some() {
-            params.push(("context", context.unwrap().to_string()));
-        }
-        if excludes.is_some() {
-            params.push(("excludes", excludes.unwrap().join(",").to_string()));
-        }
-        if fail_on_error.is_some() {
-            params.push(("fail-on-error", fail_on_error.unwrap().to_string()));
-        }
-        if forward_depth.is_some() {
-            params.push(("forward-depth", forward_depth.unwrap().to_string()));
-        }
-        if includes.is_some() {
-            params.push(("includes", includes.unwrap().join(",").to_string()));
-        }
-        if load_alternates.is_some() {
-            params.push(("load-alternates", load_alternates.unwrap().to_string()));
-        }
-        if load_images.is_some() {
-            params.push(("load-images", load_images.unwrap().to_string()));
-        }
-        if metadata_only.is_some() {
-            params.push(("metadata-only", metadata_only.unwrap().to_string()));
-        }
-        if publication_id.is_some() {
-            params.push(("publicationid", publication_id.unwrap().to_string()));
-        }
-        if version.is_some() {
-            params.push(("version", version.unwrap().to_string()));
-        }
-        if reverse_depth.is_some() {
-            params.push(("reverse-depth", reverse_depth.unwrap().to_string()));
-        }
-        if since.is_some() {
-            params.push(("since", since.unwrap().to_string()));
-        }
-        if with.is_some() {
-            params.push(("with", with.unwrap().join(",").to_string()));
-        }
-        if xref_types.is_some() {
-            params.push(("xref-types", xref_types.unwrap().join(",").to_string()));
-        }
-
         let resp = self
-            .checked_get(&Service::UriExport { member, uri }.url_path(), None, None)
+            .checked_get(
+                &Service::UriExport { member, uri }.url_path(),
+                Some(params),
+                None,
+            )
             .await?;
 
         if !(200..300).contains(&resp.status().as_u16()) {
